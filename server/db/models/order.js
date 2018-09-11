6c34819a2d68a7317fe02a13a33a83f41337b21c
Matthew Hoover <hooventud29@gmail.com>
2018-09-11 14:08:04 -0400

    added order and order item models

---
+++ server/db/models/order.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Order = db.define('order', {
+  sessionId: {
+    type: Sequelize.STRING
+  },
+  isCart: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
+});
+
+module.exports = Order;
