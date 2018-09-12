56c9a54fef4936961aab328400d35b16f16c6e9f
mbungeroth <mbungeroth@gmail.com>
2018-09-12 09:39:00 -0400

    working search in all products

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
 import Product from './Product'
 import {getProducts} from '../store/products'
 
@@ -12,22 +11,43 @@ const tempStyles = {
 class AllProducts extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      filterText: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
     this.props.getProducts()
   }
 
+  handleChange(event) {
+    this.setState({
+      filterText: event.target.value
+    })
+  }
+
   render() {
     const {products} = this.props
+    const {filterText} = this.state
     return (
       products.length && (
         <div>
           <h1>Our Products:</h1>
+          <label htmlFor="filterText">Search by name:</label>
+          <br />
+          <input
+            type="text"
+            name="filterText"
+            value={filterText}
+            onChange={this.handleChange}
+          />
           <div style={tempStyles}>
-            {products.map(product => (
-              <Product product={product} key={product.id} />
-            ))}
+            {products
+              .filter(product =>
+                product.name.toLowerCase().includes(filterText.toLowerCase())
+              )
+              .map(product => <Product product={product} key={product.id} />)}
           </div>
         </div>
       )
