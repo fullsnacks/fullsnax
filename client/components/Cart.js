66ea589399a0a8b5d49dac72b6128abf393f3233
mbungeroth <mbungeroth@gmail.com>
2018-09-16 15:25:10 -0400

    route and reducer to generate cart for logged in user, which is represented in cart component

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,33 +1,13 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchCart} from '../store/orders'
-
-const dummyData = [
-  {
-    id: 1,
-    name: 'Cheetos',
-    price: 500,
-    quantity: 1
-  },
-  {
-    id: 3,
-    name: 'Oreos',
-    price: 1000,
-    quantity: 2
-  },
-  {
-    id: 5,
-    name: 'Snufflebars',
-    price: 25000,
-    quantity: 3
-  }
-]
+import {me, getUserCart} from '../store/user'
 
 class Checkout extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      orders: []
+      cart: []
     }
     this.getCartTotal = this.getCartTotal.bind(this)
   }
@@ -38,60 +18,69 @@ class Checkout extends Component {
     }, 0)
   }
 
-  componentDidMount() {
-    //need to send guestId? to getCart function for guest cart
-    //this.props.getCart(this.props.userId)
+  async componentDidMount() {
+    await this.props.me()
+    if (this.props.user.id) {
+      const id = this.props.user.id;
+      await this.props.getUserCart(id);
+      this.setState({
+        cart: this.props.userCart,
+      })
+    }
   }
 
   render() {
-    const {cart} = this.props
-    return (
-      cart.length && (
-        <div>
-          <h4>Your current shopping cart:</h4>
-          {cart.map(item => {
-            return (
-              <div
-                key={item.id}
-                style={{display: 'flex', border: '1px solid black'}}
-              >
-                <h6 style={{margin: '15px'}}>{item.name}</h6>
-                <div style={{textAlign: 'center'}}>
-                  <h6 style={{margin: '15px'}}>Quantity:{item.quantity}</h6>
-                </div>
-                <h6 style={{margin: '15px'}}>
-                  Subtotal: ${(item.price * item.quantity / 100).toFixed(2)}
-                </h6>
+    const {cart} = this.state
+    return cart.length && (
+      <div>
+        <h4>Your current shopping cart:</h4>
+        {cart.map(item => {
+          return (
+            <div
+              key={item.name}
+              style={{display: 'flex', border: '1px solid black'}}
+            >
+              <h6 style={{margin: '15px'}}>{item.name}</h6>
+              <div style={{textAlign: 'center'}}>
+                <h6 style={{margin: '15px'}}>Quantity:{item.quantity}</h6>
               </div>
-            )
-          })}
-          <h2>
-            Your total: ${(this.getCartTotal(dummyData) / 100).toFixed(2)}
-          </h2>
-          <form action="">
-            <label htmlFor="">Street Address</label>
-            <input type="text" />
-            <label htmlFor="">City</label>
-            <input type="text" />
-            <label htmlFor="">State</label>
-            <input type="text" />
-            <label htmlFor="">Zip</label>
-            <input type="text" />
-          </form>
-          <button>CONFIRM ORDER</button>
-        </div>
-      )
+              <h6 style={{margin: '15px'}}>
+                Subtotal: ${(item.price * item.quantity / 100).toFixed(2)}
+              </h6>
+            </div>
+          )
+        })}
+        <h2>
+          Your total: ${(this.getCartTotal(cart) / 100).toFixed(2)}
+        </h2>
+        {/* <form action="">
+          <label htmlFor="">Street Address</label>
+          <input type="text" />
+          <label htmlFor="">City</label>
+          <input type="text" />
+          <label htmlFor="">State</label>
+          <input type="text" />
+          <label htmlFor="">Zip</label>
+          <input type="text" />
+        </form> */}
+        <button>CONFIRM ORDER</button>
+      </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  guestCart: state.orders.cart
+  guestCart: state.orders.cart,
   // userOrders: state.user.defaultUser.orders,
+  user: state.user.defaultUser,
+  userCart: state.user.userCart,
+  products: state.products.products,
 })
 
 const mapDispatchToProps = dispatch => ({
-  getCart: id => dispatch(fetchCart(id))
+  getCart: id => dispatch(fetchCart(id)),
+  me: () => dispatch(me()),
+  getUserCart: id => dispatch(getUserCart(id)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Checkout)
