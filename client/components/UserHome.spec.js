dabea1e4009db402b5381b9ce085fcd8224aae73
Andre Rivie <andre.rivie@gmail.com>
2018-09-17 16:12:48 -0400

    fixed merge conflicts

--- client/components/UserHome.spec.js
+++ client/components/UserHome.spec.js
@@ -16,12 +16,7 @@ describe('UserHome', () => {
     userHome = shallow(<UserHome user={firstName="Cody"} />)
   })
 
-<<<<<<< HEAD
-  xit('renders the email in an h3', () => {
-    expect(userHome.find('h3').text()).to.be.equal('Welcome, cody@email.com')
-=======
   xit('renders the name in an h2', () => {
     expect(userHome.find('h2').text()).to.be.equal('Welcome back, Cody!')
->>>>>>> andre
   })
 })
