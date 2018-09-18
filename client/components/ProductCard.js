bad047fd408769f9e2b5b42d8c52cf0f4eab1a33
mbungeroth <mbungeroth@gmail.com>
2018-09-18 10:37:50 -0400

    add stylings to all-products

---
+++ client/components/ProductCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const imgStyle = {
+
+}
+
+const Product = props => {
+  const {product} = props
+  return (
+    <div className="product-card">
+      <h4>{product.name}</h4>
+      <img src={product.imageUrl}
+        className="product-card-image"
+        alt={product.name}
+      />
+      <h4>${(product.price / 100).toFixed(2)}</h4>
+    </div>
+  )
+}
+
+export default Product
